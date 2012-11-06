974a30d3d507ba4c64b86a5191054e16d7a19401
Gabriel Oliveira <oliveira.gabriel07@gmail.com>
2012-11-06 02:20:19 -0200

    Inciando view da tela de login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,1 +1,40 @@
-Hello World!
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	background-color: #ebebeb;
+}
+.login-panel {
+	background-color: white;
+	padding: 10px;
+	position: absolute;
+	width: 300px;
+	top: 50%;
+	left: 50%;
+	margin-left: -150px;
+	height: 180px;
+	border: 1px solid #D2D2D2;
+	margin-top: -100px;
+	border-radius: 3px;
+	box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.15);
+}
+</style>
+</head>
+
+<body>
+	<div class="login-panel">
+    	<h2>Login</h2>
+        <div>
+        	<label for="username">E-mail</label>
+            <input type="text" name="username" />
+		</div>
+        <div>
+        	<label for="password">Senha</label>
+            <input type="password" name="password" />
+		</div>        
+    </div>
+</body>
+</html>
